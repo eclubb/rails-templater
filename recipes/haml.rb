833725b5df5f2c1f5bcb4fee67c463f6aca85d5b
Earle Clubb <eclubb@valcom.com>
2010-12-09 13:37:01 -0500

    Updated default application.html.haml.

--- recipes/haml.rb
+++ recipes/haml.rb
@@ -3,3 +3,4 @@ gem 'haml-rails'
 
 remove_file 'app/views/layouts/application.html.erb'
 create_file 'app/views/layouts/application.html.haml', load_template('app/views/layouts/application.html.haml','haml')
+create_file 'app/views/shared/_flash.html.haml', load_template('app/views/shared/_flash.html.haml','haml')
