b55e788cda1e9e7d1d3c8dae7b843c4f1d311805
Earle Clubb <eclubb@valcom.com>
2010-12-14 11:53:15 -0500

    Tell Compass to us SASS and inject stylesheets into layout.

--- recipes/compass.rb
+++ recipes/compass.rb
@@ -3,11 +3,24 @@ inject_into_file 'Gemfile', "\ngem 'compass'", :after => '### Application Gems #
 # TODO: support more than one framework from compass
 compass_sass_dir = 'app/stylesheets'
 compass_css_dir = 'public/stylesheets/compiled'
+compass_syntax = 'sass'
 
-compass_command = "compass init rails . --using blueprint/semantic --css-dir=#{compass_css_dir} --sass-dir=#{compass_sass_dir} "
+compass_command = 'compass init rails . --using blueprint/semantic ' +
+                  "--syntax=#{compass_syntax} " +
+                  "--css-dir=#{compass_css_dir} " +
+                  "--sass-dir=#{compass_sass_dir}"
 
 strategies << lambda do
-  puts 'Beginning Compass setup'
   run compass_command
-  puts 'Compass has been setup'
+
+  inject_string = <<-END
+    = stylesheet_link_tag 'compiled/screen.css', :media => 'screen, projection'
+    = stylesheet_link_tag 'compiled/print.css', :media => 'print'
+
+    /[if lt IE 8]
+      = stylesheet_link_tag 'compiled/ie.css', :media => 'screen, projection'
+  END
+  gsub_file 'app/views/layouts/application.html.haml',
+    "    = stylesheet_link_tag :all\n",
+    inject_string
 end
