e5e13ad8a2e610d0f73e73d7c5db0b343af5f285
Earle Clubb <eclubb@valcom.com>
2010-12-14 12:18:31 -0500

    Fixed RSpec support for DataMapper.

--- recipes/rspec.rb
+++ recipes/rspec.rb
@@ -3,8 +3,13 @@ inject_into_file 'Gemfile', "\n  gem 'rspec-rails', '~> 2.0.1'", :after => '### 
 strategies << lambda do
   generate 'rspec:install'
 
-  spec_helper_path = 'spec/spec_helper.rb'
+  if template_options[:orm] == 'datamapper'
+    spec_helper_path = 'spec/spec_helper.rb'
 
-  gsub_file spec_helper_path, 'config.fixture_path = "#{::Rails.root}/spec/fixtures"', ''
-  gsub_file spec_helper_path, /(config.use_transactional_fixtures = true)/, '# \1'
+    gsub_file spec_helper_path, /(config.fixture_path = "#\{::Rails.root\}\/spec\/fixtures")/, '# \1'
+    gsub_file spec_helper_path, /(config.use_transactional_fixtures = true)/, '# \1'
+
+    inject_into_file spec_helper_path, "\n  config.before(:suite) { DataMapper.auto_migrate! }\n",
+                     :after => /config.use_transactional_fixtures = true\n/
+  end
 end
