41bb8cc3ab6986a7cd45a47b37692c31d5cea9e4
Earle Clubb <eclubb@valcom.com>
2010-12-28 13:20:14 -0500

    Add color to bootstrap script.
    See psampaio/rails-templater.

--- bootstrap.rb
+++ bootstrap.rb
@@ -1,4 +1,5 @@
 require 'rubygems'
+require 'colored'
 require 'highline/import'
 
 rvm_lib_path = "#{`echo $rvm_path`.strip}/lib"
@@ -12,7 +13,7 @@ unless @app_name
   exit
 end
 
-puts "\nCreating Rails application \"#{@app_name}\" from template\n"
+puts "\nCreating Rails application \"#{@app_name}\" from template\n".bold
 
 rubies = RVM::list_strings
 
@@ -21,34 +22,34 @@ current_ruby = 'none' if current_ruby == nil
 selected_ruby = ''
 
 if rubies.empty?
-  puts 'Error! No rubies installed in RVM!'
+  puts 'Error! No rubies installed in RVM!'.red
   exit
 else
   choose do |menu|
     rubies.each do |r|
-      menu.choices(r) { selected_ruby = r }
+      menu.choices(r.green) { selected_ruby = r }
     end
-    menu.prompt = 'Select RVM Ruby' + ' (default: ' + current_ruby + ')'
+    menu.prompt = 'Select RVM Ruby'.yellow + ' (default: ' + current_ruby.green + ')'
   end
 end
 
-gemset_name = ask("\nWhat name should the custom gemset have?" + ' (default: ' + @app_name + ')')
+gemset_name = ask("\nWhat name should the custom gemset have?".yellow + ' (default: ' + @app_name.green + ')')
 gemset_name = @app_name if gemset_name.empty?
 
 @env = RVM::Environment.new(selected_ruby)
 
 puts "\nSetting up RVM gemset and installing bundler and rails (may take a while)..."
 
-puts "\nCreating gemset #{gemset_name} in #{selected_ruby}"
+puts "\nCreating gemset #{gemset_name} in #{selected_ruby}".yellow
 @env.gemset_create(@app_name)
 
 @rvm = "rvm #{selected_ruby}@#{gemset_name}"
 
-puts "\nInstalling bundler gem."
-puts 'Successfully installed bundler' if system "#{@rvm} gem install --no-ri --no-rdoc bundler"
+puts "\nInstalling bundler gem.".yellow
+puts 'Successfully installed bundler'.green if system "#{@rvm} gem install --no-ri --no-rdoc bundler"
 
-puts "\nInstalling rails gem."
-puts 'Successfully installed rails' if system "#{@rvm} gem install --no-ri --no-rdoc rails"
+puts "\nInstalling rails gem.".yellow
+puts 'Successfully installed rails'.green if system "#{@rvm} gem install --no-ri --no-rdoc rails"
 
 template_file = File.join(File.expand_path(File.dirname(__FILE__)), 'templater.rb')
 system "#{@rvm} exec rails new #{@app_name} -JOT -m #{template_file}"
